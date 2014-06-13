9b2b2a05ecdabd9249d2f8379d37a7049f8aab83
Frank <fschwiet@gmail.com>
2014-06-13 11:14:57 -0700

    how do I unix

--- prerun.sh
+++ prerun.sh
@@ -2,10 +2,8 @@
 
 
 # Remove the CUMULONIMBUS_FAIL_PRERUN check in real projects, its for testing.
-if $CUMULONIMBUS_FAIL_PRERUN
+if ! [ -n $CUMULONIMBUS_FAIL_PRERUN ]
 then
 	echo "Failure forced by CUMULONIMBUS_FAIL_PRERUN"
 	exit 1
 fi
-
-
