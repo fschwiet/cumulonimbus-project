f61c03b9f9f4a75a252a7f13536dd50852c2c66c
Frank <fschwiet@gmail.com>
2014-06-13 10:59:45 -0700

    make run.sh idempotent

--- run.sh
+++ run.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
-pm2 start ./src/hello-server.js --name hello-server
+id_selector="^pm2\s*id\s*:\s*\([0-9]*\)\s*$"
+old_ids=$(pm2 list -name hello-server | grep $id_selector | sed "s/$id_selector/\1/")
+
+if pm2 start ./src/hello-server.js --name hello-server
+then 
+	for old_id in $old_ids
+	do
+		pm2 delete $old_id
+	done
+else
+	echo "PM2 unable to start app"
+	exit 1
+fi
+
 
