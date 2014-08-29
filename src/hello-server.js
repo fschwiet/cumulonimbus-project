4fff7bccae06b903afd083ce4005a19ffa4f8778
Frank <fschwiet@gmail.com>
2014-08-29 12:55:01 -0700

    listen on 8088

--- src/hello-server.js
+++ src/hello-server.js
@@ -7,9 +7,9 @@ var server = http.createServer(function (request, response) {
   response.end("Hello World\nrequest.url: " + request.url + "\ncwd: " + path.resolve(".") + "\nheaders: " + JSON.stringify(request.headers));
 });
 
-// Listen on port 8089, IP defaults to 127.0.0.1
-server.listen(8089);
+// Listen on port 8088, IP defaults to 127.0.0.1
+server.listen(8088);
 
 // Put a friendly message on the terminal
-console.log("Server running at http://*:8089/");
+console.log("Server running at http://*:8088/");
 
