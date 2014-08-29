40600fcd9c9f911569b5e571b9eb6cd5ed2c9051
Frank <fschwiet@gmail.com>
2014-08-29 12:25:59 -0700

    move to port 8089, dump headers

--- src/hello-server.js
+++ src/hello-server.js
@@ -4,12 +4,12 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\nrequest.url: " + request.url + "\ncwd: " + path.resolve("."));
+  response.end("Hello World\nrequest.url: " + request.url + "\ncwd: " + path.resolve(".") + "\nheaders: " + JSON.stringify(request.headers));
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8080);
+// Listen on port 8089, IP defaults to 127.0.0.1
+server.listen(8089);
 
 // Put a friendly message on the terminal
-console.log("Server running at http://*:8080/");
+console.log("Server running at http://*:8089/");
 
