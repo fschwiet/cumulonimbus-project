ba0b7866cc3f8d4e923889897eebc4d2062cb724
fschwiet <fschwiet@gmail.com>
2015-12-31 07:02:19 -0500

    add random to diagnostic output to see caching behavior

--- src/hello-server.js
+++ src/hello-server.js
@@ -4,12 +4,16 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\nrequest.url: " + request.url + "\ncwd: " + path.resolve(".") + "\nheaders: " + JSON.stringify(request.headers));
+  response.end("Hello World\n" + 
+  	"\nrandom: " + (Math.random() * 1000) + 
+  	"\nrequest.url: " + request.url + 
+  	"\ncwd: " + path.resolve(".") + 
+  	"\nheaders: " + JSON.stringify(request.headers));
 });
 
 // Listen on port 8080, IP defaults to 127.0.0.1
 server.listen(8080);
 
 // Put a friendly message on the terminal
-console.log("Server running at http://*:8089/");
+console.log("Server running at http://*:8080/");
 
